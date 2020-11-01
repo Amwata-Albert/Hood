49622dad760dc3d79286cbf6cb53f5505a4843f3
Amwata Albert <albertotieno41@mail.com>
2020-11-01 17:52:22 +0300

    pushed to heroku

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -150,6 +150,11 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 LOGIN_REDIRECT_URL = 'index'
 
 LOGOUT_REDIRECT_URL = 'index'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
